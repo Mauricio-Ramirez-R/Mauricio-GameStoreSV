e8e4558f0b4676c17b796e53f204483ec7278cea
Mauricio Ramírez <20225526@esen.edu.sv>
2025-07-25 06:13:37 -0600

    Initial commit

---
+++ db/prisma.ts
@@ -0,0 +1,28 @@
+import { neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+
+
+neonConfig.webSocketConstructor = ws;
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString();
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString();
+        },
+      },
+    },
+  },
+});
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'GameStore SV';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'La mejor tienda para gamers, con los mejores juegos de todo el mundo';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+export const LATEST_PRODUCTS_LIMIT =
+    Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
