d74985be78a22fd61f4db9a2f524f5b131c17ce3
Mauricio Ramírez <20225526@esen.edu.sv>
2025-07-25 07:55:50 -0600

    terminando de subir todo a vercel

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Ignora errores de ESLint durante el build (útil para Vercel)
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   /* config options here */
 };
 
