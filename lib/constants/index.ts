d7eb5b3b341f5a2d39b80d9a730e42a3b26f2c93
Mauricio Ramírez <20225526@esen.edu.sv>
2025-07-29 13:53:20 -0600

    cap 7.2

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -23,3 +23,9 @@ export const shippingAddressDefaultValues = {
     postalCode: '503',
     country: 'El Salvador',
 };
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+    ? process.env.PAYMENT_METHODS.split(', ')
+    : ['PayPal', 'Stripe', 'CashOnDelivery'];
+export const DEFAULT_PAYMENT_METHOD =
+    process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
