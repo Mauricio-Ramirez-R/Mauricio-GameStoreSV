d2788f728d070cbe71841ac9da328e2eea12aa3d
Mauricio Ramírez <20225526@esen.edu.sv>
2025-08-01 09:01:31 -0600

    cambio de boton

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -65,6 +65,7 @@ const UserButton = async () => {
                     <DropdownMenuItem className='p-0 mb-1'>
                         <form action={signOutUser} className='w-full'>
                             <Button
+                                type="submit"
                                 className='w-full py-4 px-2 h-4 justify-start'
                                 variant='ghost'
                             >
